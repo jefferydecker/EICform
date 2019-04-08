0bfe5461b2584f75e9daa7eba80d7909757d8699
Don Erickson <36769995+dlerickson@users.noreply.github.com>
2019-04-07 21:54:25 -0500

    Create Project
    So this is my attempt at cloning the empty repo and building into a Spring Boot project.

---
+++ SpringBootWebApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringBootWebApplication {
+
+	@SpringBootApplication
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(SpringBootWebApplication.class, args)
+
+	}
+
+}
